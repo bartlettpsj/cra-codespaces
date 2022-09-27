9b8435f64265e5c171c18beb40bc15da00fdd839
bartlettpsj <paul@infoqss.com>
2022-09-27 22:47:32 +0000

    asasdsad

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.JS 18.0 in your favorite browser
+          Learn React.JS 18.5 in your favorite browser dynamically
         </a>
         <Paul greeting='Good Night, sleep tight, sweet dreams!'/>
       </header>
